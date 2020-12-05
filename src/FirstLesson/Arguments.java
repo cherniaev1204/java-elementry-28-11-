cd0ed6b2c6964df7154499e0a6b807643644c128
Алексей <alex.your03@gmail.com>
2020-12-05 16:13:01 +0200

    Add lesson2

--- src/FirstLesson/Arguments.java
+++ src/FirstLesson/Arguments.java
@@ -2,7 +2,7 @@ package FirstLesson;
 
 public class Arguments {
     public static void main(String[] args) {
-        for (String arg : args){
+        for (String arg : args) {
             System.out.println("Program argument: " + arg);
         }
     }
---
+++ src/lesson2/StringTest.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String string = "";
+        String string1 = "Some text";
+        String string2 = new String("Test");
+
+        System.out.println(string);
+        System.out.println(string1);
+        System.out.println(string2);
+
+        StringBuffer buffer = new StringBuffer("Test");
+
+        buffer.append("continue");
+        buffer.append("ttt");
+        System.out.println("StringBuffer append " + buffer.toString());
+
+    }
+}
