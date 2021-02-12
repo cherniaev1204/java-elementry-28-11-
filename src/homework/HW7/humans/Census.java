c42aa9d12b6aa2cd4e33a9760e6d41795a3c04f0
cherniaev1204 <crazyblackhare@gmail.com>
2021-02-12 18:20:33 +0200

    reworked homework

--- src/homework/HW7/humans/Census.java
+++ src/homework/HW7/humans/Census.java
@@ -10,9 +10,24 @@ import java.util.Map;
 public class Census {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String read;
+
         Map<String,String> census = new HashMap<>();
 
-
+        while(true){
+            System.out.println("Введите город:");
+            String city = reader.readLine();
+            if (city.isEmpty()){
+                break;
+            }
+            System.out.println("Введите фамилию:");
+            String surname = reader.readLine();
+            if (surname.isEmpty()){
+                break;
+            }
+            census.put(city,surname);
+        }
+        System.out.println("Введите город:");
+        String findYourCity = reader.readLine();
+        System.out.println(census.getOrDefault(findYourCity, "Такого города нет"));
     }
 }
