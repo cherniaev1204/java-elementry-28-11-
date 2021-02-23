912e8135bdd9f44abc087b3334e791505845bc9a
cherniaev1204 <crazyblackhare@gmail.com>
2021-02-23 17:54:35 +0200

    finally!!!

--- src/homework/task24/FileCopy.java
+++ src/homework/task24/FileCopy.java
@@ -1,25 +1,34 @@
 package homework.task24;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class FileCopy {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName;
-        FileInputStream inputStream;
+        FileInputStream fileInputStream;
         String copyFile;
         try {
-            System.out.println("File name which you will copy:");
+            System.out.println("Type file name which you will copy:");
             fileName = reader.readLine();
-            inputStream = new FileInputStream(fileName);
+            fileInputStream = new FileInputStream(fileName);
+
         } catch (IOException e) {
             System.out.println("Файл не существует.");
             System.out.println("Write file name one more time");
             fileName = reader.readLine();
-            inputStream = new FileInputStream(fileName);
+            fileInputStream = new FileInputStream(fileName);
+
         }
         System.out.println("Type file name in which you will copy: ");
         copyFile = reader.readLine();
-        FileOutputStream stream = new FileOutputStream(copyFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(copyFile);
+        while(fileInputStream.available() > 0){
+            int text = fileInputStream.read();
+            fileOutputStream.write(text);
+        }
+        fileOutputStream.close();
+        fileInputStream.close();
     }
 }
